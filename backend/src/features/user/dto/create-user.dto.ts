b3e275fa846dc2450b6e2291607547b229c31e74
YKuts <yuliiakuts@gmail.com>
2023-10-03 23:54:11 +0200

    Test table Users

--- backend/src/features/user/dto/create-user.dto.ts
+++ backend/src/features/user/dto/create-user.dto.ts
@@ -1,1 +1,5 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    first_name;
+    last_name;
+    email;
+}
--- backend/src/features/user/entities/user.entity.ts
+++ backend/src/features/user/entities/user.entity.ts
@@ -1,1 +1,7 @@
-export class User {}
+export class User {
+    user_id;
+    first_name;
+    last_name;
+    email;
+    created_at;
+}
