badd3125360a2ff365b180dcbeedf39d549fa6e7
yuliiak <98550411+ykuts@users.noreply.github.com>
2023-10-11 16:02:17 +0200

    Update user.entity.ts

--- backend/src/features/users/entities/user.entity.ts
+++ backend/src/features/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+
+  @Column({ type: 'varchar' })
+  user_name: string;
+
+  @Column({ type: 'varchar' })
+  user_email: string;
+
+  @Column({ type: 'varchar' })
+  role: string;
+
+  @Column({ type: 'timestamp' })
+  updated_at: string;
+
+  @Column({ type: 'varchar' })
+  updated_by: string;
+}
