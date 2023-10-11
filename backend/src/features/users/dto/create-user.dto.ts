aa2272cec7201c0d27fd71784870025ef478f4c8
yuliiak <98550411+ykuts@users.noreply.github.com>
2023-10-11 16:09:36 +0200

    Update create-user.dto.ts

--- backend/src/features/users/dto/create-user.dto.ts
+++ backend/src/features/users/dto/create-user.dto.ts
@@ -1,1 +1,34 @@
-export class CreateUserDto {}
+import {
+  IsAlphanumeric,
+  IsEmail,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2, { message: 'Name must have atleast 2 characters.' })
+  @IsAlphanumeric(null, {
+    message: 'Username does not allow other than alpha numeric chars.',
+  })
+  @IsNotEmpty()
+  user_name: string;
+
+  @IsNotEmpty()
+  @IsEmail(null, { message: 'Please provide valid Email.' })
+  user_email: string;
+
+  @IsString()
+  @IsEnum(['economist', 'gatekeeper', 'user'])
+  role: string;
+
+  @IsString() //need to be changed
+  updated_at: string;
+
+  @IsString() //need to be changed
+  updated_by: string;
+}
